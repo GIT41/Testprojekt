f138b5247cfdf0e5b2a303264ff2ededeb6ea8ab
Selina <semakasa@outlook.de>
2016-06-10 12:02:11 +0200

    Implementation of the selection sort

--- Testprojekt/src/testprojekt/Rechner.java
+++ Testprojekt/src/testprojekt/Rechner.java
@@ -17,4 +17,18 @@ public float malzwei(float[] numbers)
 float doppelt = 2*numbers[0];
 return doppelt;   
 }
+
+public float[] selection(float[] numbers){
+    float temp;
+    for(int i = 0; i < numbers.length - 1; i++){
+        for (int j = i + 1; j < numbers.length; j++){
+            if (numbers[j] < numbers[i]){
+                temp = numbers[j];
+                numbers[j] = numbers[i];
+                numbers[i] = temp;
+            }
+        }
+    }
+    return numbers;
+} 
 }
